4d57e70309914fd83201815e09a2e5d5e8498b51
Anibal Santos <anibalsantosgo@gmail.com>
2021-06-01 19:44:13 +0200

    pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,21 @@ import { BeerComponent } from './components/beer/beer.component';
 
 import { HttpClientModule } from '@angular/common/http';
 import { HeroComponent } from './components/hero/hero.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 @NgModule({
   declarations: [AppComponent, BeersComponent, BeerComponent, HeroComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
