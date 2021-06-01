c4f2224da90b01db1e4685d3335380e49ecf80e8
Anibal Santos <anibalsantosgo@gmail.com>
2021-06-01 18:26:09 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BeersComponent } from './components/beers/beers.component';
+import { BeerComponent } from './components/beer/beer.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { HeroComponent } from './components/hero/hero.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, BeersComponent, BeerComponent, HeroComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
